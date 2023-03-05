229574a24d9d79a7fcae4ef296fc3113bae60a15
Inwerwm <saclainvervm@gmail.com>
2023-03-05 11:19:09 +0900

    チャットログの表示機能を作成

--- AIChat/ViewModels/ChatGptViewModel.cs
+++ AIChat/ViewModels/ChatGptViewModel.cs
@@ -1,10 +1,32 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using AIChat.Core.Models.ChatGpt.Data;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace AIChat.ViewModels;
 
-public class ChatGptViewModel : ObservableRecipient
+public partial class ChatGptViewModel : ObservableRecipient
 {
+    [ObservableProperty]
+    private string _inputText;
+
+    public ObservableCollection<Choice> Choices
+    {
+        get;
+    }
+
     public ChatGptViewModel()
     {
+        _inputText = string.Empty;
+        Choices = new();
+    }
+
+    [RelayCommand]
+    private void AddChat()
+    {
+        Choices.Add(new(0, new(
+            "User",
+            Guid.NewGuid().ToString()
+            ), "test"));
     }
 }
