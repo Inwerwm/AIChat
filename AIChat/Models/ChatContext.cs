1920b86dde353979e46d0cfa045bb552552617e9
Inwerwm <saclainvervm@gmail.com>
2023-03-09 23:32:59 +0900

    チャットコンテキスト選択をListViewにしてリネームできるようにした

--- AIChat/Models/ChatContext.cs
+++ AIChat/Models/ChatContext.cs
@@ -1,11 +1,15 @@
 ﻿using AIChat.Core.Models.ChatGpt;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml.Controls;
 
 namespace AIChat.Models;
 public partial class ChatContext : ObservableRecipient
 {
     [ObservableProperty]
     private string _name;
+    [ObservableProperty]
+    private bool _enableRenameMode;
 
     public ChatGptContext Context
     {
@@ -16,5 +20,16 @@ public partial class ChatContext : ObservableRecipient
     {
         _name = name;
         Context = context;
+    }
+
+    [RelayCommand]
+    private void ToggleRenameMode()
+    {
+        EnableRenameMode = !EnableRenameMode;
+    }
+
+    public void TextBox_LostFocus(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        ToggleRenameMode();
     }
 }
